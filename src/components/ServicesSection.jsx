2f2bb8a975c10b6d469de1022ced468a3195858a
sathyajit-P <sudupa2005@gmail.com>
2024-10-18 11:27:25 +0530

    linked to services

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const services = [
   {
@@ -24,23 +25,24 @@ const ServicesSection = () => {
       <h2 className="text-white text-4xl font-bold text-center mb-20">Services That Go the Extra Mile</h2>
       <div className="min-h-40 grid grid-cols-1 md:grid-cols-3 justify-items-center mx-6 mt-8 px-4 md:px-0">
         {services.map((service, index) => (
-          <div 
-            key={index}
-            className="max-w-sm rounded overflow-hidden shadow-lg border border-[#ffffff1a] bg-gradient-to-r from-[#060606] to-[#0e0e0e] mb-6">
+          <Link to='/services' key={index} className='no-underline'>
+            <div
+              className="max-w-sm rounded overflow-hidden shadow-lg border border-[#ffffff1a] bg-gradient-to-r from-[#060606] to-[#0e0e0e] mb-6">
               {/* {console.log(service.image)} */}
-            <img src={service.image} alt={service.title} className='w-[80px] h-[100px] ml-4 overflow-auto mb-10' />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl text-white mb-2">{service.title}</div>
-              <p className="text-white text-base">
-                {service.description}.
-              </p>
-            </div>
+              <img src={service.image} alt={service.title} className='w-[80px] h-[100px] ml-4 overflow-auto mb-10' />
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl text-white mb-2">{service.title}</div>
+                <p className="text-white text-base">
+                  {service.description}.
+                </p>
+              </div>
             {/* <div class="px-6 pt-4 pb-2">
               <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
               <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
               <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
             </div> */}
-          </div>
+            </div>
+          </Link>
         ))}
       </div>
     </>
---
+++ src/components/clients.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const clients = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default clients
